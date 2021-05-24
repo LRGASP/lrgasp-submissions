b14790f771ab8720da39dd91a7db6a06c1c66b6d
Mark Diekhans <markd@ucsc.edu>
2021-05-24 14:16:26 -0700

    improve and speed up detection of errors in expersssion matrices

--- lib/lrgasp/expression_data.py
+++ lib/lrgasp/expression_data.py
@@ -2,33 +2,27 @@
 expression matrix parser/validator
 """
 import csv
-import pandas as pd
 import numpy as np
+import pandas as pd
 from lrgasp import LrgaspException
 from lrgasp import gopen
 from lrgasp.defs import validate_feature_ident
 
-def check_row_consistency(expression_tsv):
-    """Pandas just pads or ignores inconsistent number of columns,
-    so to validate the matrix is consistent, we need to make another pass.
-    """
-    # note: if pandas was given a header, it will error on long rows, but not short ones
-    minLen = maxLen = None
-    with gopen(expression_tsv) as fh:
-        for row in csv.reader(fh, dialect=csv.excel_tab):
-            if minLen is None:
-                minLen = maxLen = len(row)
-            else:
-                minLen = min(len(row), minLen)
-                maxLen = max(len(row), maxLen)
-    if minLen is None:
-        raise LrgaspException("TSV is empty")
-    if minLen != maxLen:
-        raise LrgaspException(f"TSV has an inconsistent number of columns: min={minLen}, max={maxLen}")
+##
+# In order to detect empty columns and short rows, disallow space as a NA value, as well
+# as remove other weirder values
+##
+na_values = frozenset(['NaN', 'NA', 'nan', 'null', '-nan', 'n/a', 'N/A', 'NULL', '-NaN'])
 
-def sample_column_names(expression):
+
+class ExpressionMatrix:
+    """Expression matrix and operations to access columns by file or biosample accession"""
+    def __init__(self, mat):
+        self.mat = mat
+
+def sample_column_names(expression_mat):
     "return all but ID column"
-    return [col for col in expression.columns if col != "ID"]
+    return [col for col in expression_mat.columns if col != "ID"]
 
 def validate_header(expression_mat):
     if "ID" not in expression_mat.columns:
@@ -39,7 +33,7 @@ def validate_header(expression_mat):
 def check_column_type(expression_mat, col):
     "validate column type is correct"
     if expression_mat.dtypes[col] not in (np.float64, np.int64):
-        raise LrgaspException(f"Invalid value(s) in column '{col}', must be a number or NA, appears to contain other types, induced type is '{expression_mat.dtypes[col]}'")
+        raise LrgaspException(f"Invalid value(s) in column '{col}', must be a number, NA, or NaN, appears to contain other types or a short row")
 
 def validate_data(expression_mat):
     if expression_mat.size == 0:
@@ -50,12 +44,15 @@ def validate_data(expression_mat):
 
 def load(expression_tsv):
     try:
-        check_row_consistency(expression_tsv)
         with gopen(expression_tsv) as fh:
+            # na_value handling is needed to detect empty cells and short
+            # rows, preventing implicit conversion to NaN.
             expression_mat = pd.read_csv(fh,
                                          dialect=csv.excel_tab,
                                          header=0,
-                                         converters={"ID": str})
+                                         converters={"ID": str},
+                                         na_values=na_values,
+                                         keep_default_na=False)
             validate_header(expression_mat)
             expression_mat.set_index("ID", drop=False, inplace=True, verify_integrity=True)
             validate_data(expression_mat)
